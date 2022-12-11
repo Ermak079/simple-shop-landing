0925edb4b7e7f3535bb93de2eb5d27681bc4406d
Roman <varermak@gmail.com>
2022-12-11 14:33:01 +0300

    Добавление, изменение и удаление продукта

--- app/controllers/ProductsController.php
+++ app/controllers/ProductsController.php
@@ -16,4 +16,106 @@ class ProductsController extends ControllerBase
             'sections' => $result
         ];
     }
+
+    public function addProductAction()
+    {
+        $this->isApiAction = true;
+        $obj = $this->request->getJsonRawBody();
+        $token = $obj->token;
+        $admin = Admins::findFirst([
+            'token = :token:',
+            'bind' => [
+                'token' => $token
+            ]
+        ]);
+        if (!$admin){
+            return[
+                '_error' => 'Нет прав для данного действия'
+            ];
+        }
+        $product = new Products();
+        $product->name = "Новый товар";
+        $product->save();
+        return [
+            '_status' => true,
+            'section' => $product->name
+        ];
+    }
+
+    public function removeProductAction()
+    {
+        $this->isApiAction = true;
+        $obj = $this->request->getJsonRawBody();
+        $token = $obj->token;
+        $admin = Admins::findFirst([
+            'token = :token:',
+            'bind' => [
+                'token' => $token
+            ]
+        ]);
+        if (!$admin){
+            return[
+                '_error' => 'Нет прав для данного действия'
+            ];
+        }
+        $prod_name = $obj->prouct_name;
+        $product = Products::findFirst([
+            'product_name = :product_name:',
+            'bind' => [
+                'product_name' => $prod_name
+            ]
+        ]);
+
+        if (!$product){
+            return [
+                '_error' => 'Нет такого товарова -> ' . $prod_name
+            ];
+        }
+        $old_name = $product->name;
+        $new_prod_name = $obj->new_product_name;
+        $product->name = $new_prod_name;
+        $product->save();
+
+        return [
+            '_status' => true,
+            'old_name' => $old_name,
+            'new_name' => $product->name
+        ];
+    }
+
+    public function deleteProductAction()
+    {
+        $this->isApiAction = true;
+        $obj = $this->request->getJsonRawBody();
+        $token = $obj->token;
+        $admin = Admins::findFirst([
+            'token = :token:',
+            'bind' => [
+                'token' => $token
+            ]
+        ]);
+        if (!$admin){
+            return[
+                '_error' => 'Нет прав для данного действия'
+            ];
+        }
+        $prod_name = $obj->product_name;
+        $product = Products::findFirst([
+            'name = :name:',
+            'bind' => [
+                'name' => $prod_name
+            ]
+        ]);
+        if (!$product){
+            return [
+                '_status' => true
+            ];
+        }
+        $product->delete();
+        return [
+            '_status' => true
+        ];
+    }
+
+
 }
